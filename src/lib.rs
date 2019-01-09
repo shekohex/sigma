e08a4e080f499e0f3f0c9b6e4d5d00d4cc6cd6ab
Shady Khalifa <shekohex@gmail.com>
2019-01-09 03:44:02 +0200

    add simple setup

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,21 @@
+use pest::Parser;
+use pest_derive::Parser;
+
+#[derive(Parser)]
+#[grammar = "sigma.pest"]
+struct SigmaParser;
+
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
     fn it_works() {
+        let input = "test {{ test: u32 }}";
+        let output = SigmaParser::parse(Rule::sigma, &input)
+            .unwrap()
+            .next()
+            .unwrap();
+        println!("{}", output);
         assert_eq!(2 + 2, 4);
     }
 }
