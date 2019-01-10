566c70c647d25418c3e4cf3419957490e3db5a8e
Shady Khalifa <shekohex@gmail.com>
2019-01-10 22:41:18 +0200

    simple prototype

--- src/lib.rs
+++ src/lib.rs
@@ -4,25 +4,20 @@
 
 //! Sigma
 
-use pest::{error::Error, iterators::Pair, Parser};
-use pest_derive::Parser;
+mod parser;
 
-#[derive(Parser)]
-#[grammar = "sigma.pest"]
-struct SigmaParser;
+use crate::parser::{Rule, SigmaParser};
+use pest::{
+  error::{Error as PestError, ErrorVariant},
+  iterators::Pair,
+  Parser, Position, Span,
+};
+use std::collections::HashMap;
 
-enum SigmaError {
-  ParserError,
-}
-
-#[derive(Clone, Debug, PartialEq, Eq)]
-enum Expression<'e> {
-  Variable(Variable<'e>),
-  Function(Function<'e>),
-}
+type SigmaResult<'a, T> = Result<T, PestError<Rule>>;
 
 #[derive(Clone, Debug, PartialEq, Eq)]
-enum DataType {
+pub enum DataType {
   U8,
   I8,
   U16,
@@ -31,100 +26,237 @@ enum DataType {
   I32,
   U64,
   I64,
+  F32,
+  F64,
   Bool,
   Str,
 }
 
-impl From<&str> for DataType {
-  fn from(val: &str) -> DataType {
-    use self::DataType::*;
-    match val {
-      "u8" => U8,
-      "i8" => I8,
-      "u16" => U16,
-      "i16" => I16,
-      "u32" => U32,
-      "i32" => I32,
-      "u64" => U64,
-      "i64" => I64,
-      "bool" => Bool,
-      "str" => Str,
-      _ => unreachable!(),
-    }
-  }
-}
-
 #[derive(Clone, Debug, Default, PartialEq, Eq)]
-struct Variable<'a> {
+pub struct Variable<'a> {
   pub name: &'a str,
   pub nullable: bool,
-  pub data_type: Option<DataType>,
+  pub typed: bool,
+  pub data_type: Option<(DataType, Span<'a>)>,
+  pub location: (usize, usize),
+  pub functions: Vec<(&'a str, Span<'a>)>,
+  pub current_value: String,
+  pub name_span: Option<Span<'a>>,
 }
 
-#[derive(Clone, Debug, Default, PartialEq, Eq)]
-struct Function<'a> {
-  pub name: &'a str,
-  pub arg_count: u8,
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct Function<'b> {
+  pub name: &'b str,
+  pub pure: bool,
+  pub call: fn(String) -> String,
 }
 
-fn parse(input: &str) -> Result<Vec<Expression>, Error<Rule>> {
-  let mut result = Vec::new();
-  for sigma in SigmaParser::parse(Rule::sigma, input)? {
-    if sigma.as_rule() == Rule::var_pair {
-      let var = parse_var_pair(sigma);
-      let expr = Expression::Variable(var);
-      result.push(expr);
+#[derive(Debug, PartialEq, Eq, Default)]
+pub struct Sigma<'s> {
+  vars: HashMap<&'s str, Variable<'s>>,
+  registry: HashMap<&'s str, &'s str>,
+  input: String,
+  functions: HashMap<&'s str, Function<'s>>,
+}
+
+impl<'s> Sigma<'s> {
+  pub fn new() -> Self {
+    let mut functions = HashMap::new();
+
+    let upper_case_fn = Function {
+      name: "UPPERCASE",
+      pure: true,
+      call: |input| input.to_uppercase(),
+    };
+    functions.insert("UPPERCASE", upper_case_fn);
+    Self {
+      input: String::new(),
+      vars: HashMap::new(),
+      functions,
+      registry: HashMap::new(),
     }
   }
-  Ok(result)
+
+  /// Parse the input and return expressions
+  pub fn parse(mut self, input: &'s str) -> SigmaResult<'s, Self> {
+    for sigma in SigmaParser::parse(Rule::sigma, &input)? {
+      if sigma.as_rule() == Rule::var_pair {
+        let var = parse_var_pair(sigma)?;
+        self.vars.insert(var.name, var);
+      }
+    }
+    self.input = input.into();
+    Ok(self)
+  }
+
+  pub fn register(mut self, key: &'s str, value: &'s str) -> Self {
+    self.registry.insert(key, value);
+    self
+  }
+
+  pub fn compile(mut self) -> SigmaResult<'s, String> {
+    for var in self.vars.values_mut() {
+      if let Some(value) = self.registry.get(var.name) {
+        let mut current_data = (*value).to_owned();
+        for function in &var.functions {
+          let f = self.functions.get(&function.0).ok_or_else(|| {
+            PestError::new_from_span(
+              ErrorVariant::CustomError {
+                message: format!("undefined function: {}", function.0),
+              },
+              function.1.clone(),
+            )
+          })?;
+          current_data = (f.call)(current_data);
+        }
+        var.current_value = current_data;
+        self
+          .input
+          .replace_range(var.location.0..var.location.1, &var.current_value);
+      } else if var.nullable {
+        self.input.replace_range(var.location.0..var.location.1, "");
+      } else {
+        return Err(PestError::new_from_span(
+          ErrorVariant::CustomError {
+            message: format!(
+              "undefined variable: {} consider adding a register for it",
+              var.name
+            ),
+          },
+          var.name_span.clone().unwrap(),
+        ));
+      }
+    }
+    Ok(self.input)
+  }
 }
 
-fn parse_var_pair(pair: Pair<Rule>) -> Variable {
-  let mut inner_rules = pair.into_inner();
-  let _open_pairs = inner_rules.next().unwrap();
-  let var = inner_rules.next().unwrap();
-  let mut var_inner = var.into_inner();
-  let var_name = var_inner.next().unwrap();
+// TODO: Refactor this function
+// TODO: Validate Datatypes
+// TODO: Parse functions in seprate function
+fn parse_var_pair(pair: Pair<Rule>) -> SigmaResult<Variable> {
   let mut variable = Variable::default();
-  variable.name = var_name.as_str();
-  if let Some(nullable_or_datatype) = var_inner.next() {
-    match nullable_or_datatype.as_rule() {
+  let mut inner_rules = pair.into_inner();
+  let open_pairs = inner_rules.next().unwrap();
+  let var = inner_rules.next().unwrap();
+  let var_inner = var.into_inner();
+  for var_rules in var_inner {
+    match var_rules.as_rule() {
       Rule::nullable => {
         variable.nullable = true;
       },
+      Rule::var_name => {
+        variable.name = var_rules.as_str();
+        variable.name_span = Some(var_rules.as_span());
+      },
+      Rule::data_type_sep => {
+        // it must has data type then
+        variable.typed = true;
+      },
       Rule::data_type => {
-        let data_type = nullable_or_datatype.as_str();
-        variable.data_type = Some(data_type.into());
+        let data_type = var_rules;
+        variable.data_type =
+          Some((parse_data_type(&data_type)?, data_type.as_span()));
       },
       _ => {},
     };
   }
-  variable
+
+  let close_pairs_or_functions = inner_rules;
+  for pair in close_pairs_or_functions {
+    let rule = pair.as_rule();
+    match rule {
+      Rule::function => {
+        if variable.data_type.is_none() || !variable.typed {
+          return Err(PestError::new_from_span(
+            ErrorVariant::ParsingError {
+              positives: vec![Rule::data_type],
+              negatives: vec![],
+            },
+            variable.name_span.unwrap(),
+          ));
+        }
+        let mut function = pair.into_inner();
+        let _sep = function.next().unwrap();
+        let function_name = function.next().unwrap();
+        variable
+          .functions
+          .push((function_name.as_str(), function_name.as_span()));
+      },
+      Rule::pair_close => {
+        variable.location =
+          (open_pairs.as_span().start(), pair.as_span().end());
+        break;
+      },
+      _ => {},
+    };
+  }
+  // data type check
+  if variable.typed && variable.data_type.is_none() {
+    return Err(PestError::new_from_span(
+      ErrorVariant::ParsingError {
+        positives: vec![Rule::data_type],
+        negatives: vec![],
+      },
+      variable.name_span.clone().unwrap(),
+    ));
+  }
+  Ok(variable)
 }
+
+fn parse_data_type<'b>(pair: &Pair<Rule>) -> SigmaResult<'b, DataType> {
+  use self::DataType::*;
+  let val = pair.as_str();
+  let result = match val {
+    "u8" => U8,
+    "i8" => I8,
+    "u16" => U16,
+    "i16" => I16,
+    "u32" => U32,
+    "i32" => I32,
+    "u64" => U64,
+    "i64" => I64,
+    "f32" => F32,
+    "f64" => F64,
+    "bool" => Bool,
+    "str" => Str,
+    _ => {
+      return Err(PestError::new_from_span(
+        ErrorVariant::CustomError {
+          message: format!("Unknown Data type: {}", val),
+        },
+        pair.as_span(),
+      ));
+    },
+  };
+  Ok(result)
+}
+
 #[cfg(test)]
 mod tests {
   use super::*;
   #[test]
-  fn it_works() {
-    let input = "test {{ optinal? }} and {{ without_type }} and even with pipelines {{ username: u32 | FN_NAME }}";
-    let output = parse(input).unwrap();
-    let expected = [
-      Expression::Variable(Variable {
-        name: "optinal",
-        nullable: true,
-        data_type: None,
-      }),
-      Expression::Variable(Variable {
-        name: "without_type",
-        nullable: false,
-        data_type: None,
-      }),
-      Expression::Variable(Variable {
-        name: "username",
-        nullable: false,
-        data_type: Some(DataType::U32),
-      }),
-    ];
-    assert_eq!(output, expected.to_vec());
+  #[should_panic]
+  fn missing_data_type() {
+    let input = "{{ username: }}";
+    let _ = Sigma::new()
+      .parse(input)
+      .unwrap()
+      .register("username", "test")
+      .compile()
+      .unwrap();
+  }
+
+  #[test]
+  #[should_panic]
+  fn unknown_data_type() {
+    let input = "{{ username: unknown }}";
+    let output = Sigma::new()
+      .parse(input)
+      .unwrap()
+      .register("username", "test")
+      .compile()
+      .unwrap();
+    println!("{:?}", output);
   }
 }
