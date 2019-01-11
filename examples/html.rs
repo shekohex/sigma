cdd1dbb5b246fc66ccd3a5924667283b2e681377
Shady Khalifa <shekohex@gmail.com>
2019-01-11 17:29:54 +0200

    add html example

---
+++ examples/html.rs
@@ -0,0 +1,23 @@
+use sigma::Sigma;
+use std::{collections::HashMap, fs::File, io::Read};
+fn main() -> Result<(), ()> {
+  let mut html_file =
+    File::open("./examples/data/hello.html").expect("hello.html not found !");
+  let mut input = String::new();
+  html_file
+    .read_to_string(&mut input)
+    .expect("unable to read the file");
+  let mut data = HashMap::new();
+  data.insert("name".to_owned(), "someone".to_owned());
+  data.insert("id".to_owned(), "100".to_owned());
+  data.insert("title".to_owned(), "Home Page".to_owned());
+  let result = Sigma::new(&input)
+    .bind_map(data)
+    .parse()
+    .map_err(|e| eprintln!("Parse Error:\n{}", e))? // error handling..
+    .compile()
+    .map_err(|e| eprintln!("Compile Error:\n{}", e))?; // error handling..
+  
+  println!("{}", result);
+  Ok(())
+}
