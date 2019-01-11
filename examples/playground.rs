0414a2f5dc7912cd224f93b8a469762e8842013e
Shady Khalifa <shekohex@gmail.com>
2019-01-11 17:29:21 +0200

    update api

--- examples/playground.rs
+++ examples/playground.rs
@@ -1,12 +1,11 @@
 use sigma::Sigma;
 fn main() -> Result<(), ()> {
-  let result = Sigma::new()
-    .parse("my username is {{ username: str | UPPERCASE }} WOW!")
-    .map_err(|e| println!("{}", e))? // error handling..
-    .register("username", "shekohex")
-    .compile()
-    .map_err(|e| println!("{}", e))?; // error handling..
-
+  let result = Sigma::new("my username is {{ username: u32 | UPPERCASE }} WOW!")
+      .bind("username", "shekohex")
+      .parse()
+      .map_err(|e| eprintln!("Parse Error:\n{}", e))? // error handling..
+      .compile()
+      .map_err(|e| eprintln!("Compile Error:\n{}", e))?; // error handling..
   assert_eq!("my username is SHEKOHEX WOW!", result);
   Ok(())
 }
