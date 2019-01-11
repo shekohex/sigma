58634919c1ee957756ea3f088846e41a781e7c1d
Shady Khalifa <shekohex@gmail.com>
2019-01-11 13:34:33 +0200

    add extra help in typos

--- src/lib.rs
+++ src/lib.rs
@@ -100,26 +100,39 @@ mod parser;
 use crate::parser::{Rule, SigmaParser};
 use pest::{
   error::{Error as PestError, ErrorVariant},
-  iterators::Pair,
+  iterators::{Pair, Pairs},
   Parser, Span,
 };
 use std::collections::HashMap;
 
 type SigmaResult<'a, T> = Result<T, PestError<Rule>>;
 
+/// Primitive Data Types
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub enum DataType {
+  /// The 8-bit unsigned integer type.
   U8,
+  /// The 8-bit signed integer type.
   I8,
+  /// The 16-bit unsigned integer type.
   U16,
+  /// The 16-bit signed integer type.
   I16,
+  /// The 32-bit unsigned integer type.
   U32,
+  /// The 32-bit signed integer type.
   I32,
+  /// The 64-bit unsigned integer type.
   U64,
+  /// The 64-bit signed integer type.
   I64,
+  /// The 32-bit floating point type.
   F32,
+  /// The 64-bit floating point type.
   F64,
+  /// The boolean type.
   Bool,
+  /// String.
   Str,
 }
 
@@ -131,7 +144,6 @@ pub struct Variable<'a> {
   pub data_type: Option<(DataType, Span<'a>)>,
   pub location: (usize, usize),
   pub functions: Vec<(&'a str, Span<'a>)>,
-  pub current_value: String,
   pub name_span: Option<Span<'a>>,
 }
 
@@ -179,8 +191,7 @@ impl<'s> Sigma<'s> {
   pub fn parse(mut self, input: &'s str) -> SigmaResult<'s, Self> {
     for sigma in SigmaParser::parse(Rule::sigma, &input)? {
       if sigma.as_rule() == Rule::var_pair {
-        let var = parse_var_pair(sigma)?;
-        self.vars.insert(var.name, var);
+        self.parse_var_pair(sigma)?;
       }
     }
     self.input = input.into();
@@ -193,33 +204,29 @@ impl<'s> Sigma<'s> {
   }
 
   pub fn compile(mut self) -> SigmaResult<'s, String> {
-    for var in self.vars.values_mut() {
+    for var in self.vars.values() {
       if let Some(value) = self.registry.get(var.name) {
         let mut current_data = (*value).to_owned();
         for function in &var.functions {
-          let f = self.functions.get(&function.0).ok_or_else(|| {
-            PestError::new_from_span(
-              ErrorVariant::CustomError {
-                message: format!("undefined function: {}", function.0),
-              },
-              function.1.clone(),
-            )
-          })?;
+          let f = &self.functions[&function.0]; // we are sure it will be there.
           current_data = (f.call)(current_data);
         }
-        var.current_value = current_data;
         self
           .input
-          .replace_range(var.location.0..var.location.1, &var.current_value);
+          .replace_range(var.location.0..var.location.1, &current_data);
       } else if var.nullable {
         self.input.replace_range(var.location.0..var.location.1, "");
       } else {
+        let mut extra_help = String::new();
+        if let Some(matches) = parser::did_you_mean(var.name, self.vars.keys())
+        {
+          extra_help = format!("Did you mean: `{}` ?", matches);
+        } else {
+          extra_help = "consider adding a register for it".to_owned();
+        }
         return Err(PestError::new_from_span(
           ErrorVariant::CustomError {
-            message: format!(
-              "undefined variable: {} consider adding a register for it",
-              var.name
-            ),
+            message: format!("undefined variable: {} {}", var.name, extra_help),
           },
           var.name_span.clone().unwrap(),
         ));
@@ -227,107 +234,144 @@ impl<'s> Sigma<'s> {
     }
     Ok(self.input)
   }
-}
 
-// TODO: Refactor this function
-// TODO: Validate Datatypes
-// TODO: Parse functions in seprate function
-fn parse_var_pair(pair: Pair<Rule>) -> SigmaResult<Variable> {
-  let mut variable = Variable::default();
-  let mut inner_rules = pair.into_inner();
-  let open_pairs = inner_rules.next().unwrap();
-  let var = inner_rules.next().unwrap();
-  let var_inner = var.into_inner();
-  for var_rules in var_inner {
-    match var_rules.as_rule() {
-      Rule::nullable => {
-        variable.nullable = true;
-      },
-      Rule::var_name => {
-        variable.name = var_rules.as_str();
-        variable.name_span = Some(var_rules.as_span());
-      },
-      Rule::data_type_sep => {
-        // it must has data type then
-        variable.typed = true;
-      },
-      Rule::data_type => {
-        let data_type = var_rules;
-        variable.data_type =
-          Some((parse_data_type(&data_type)?, data_type.as_span()));
-      },
-      _ => {},
-    };
-  }
-
-  let close_pairs_or_functions = inner_rules;
-  for pair in close_pairs_or_functions {
-    let rule = pair.as_rule();
-    match rule {
-      Rule::function => {
-        if variable.data_type.is_none() || !variable.typed {
-          return Err(PestError::new_from_span(
-            ErrorVariant::ParsingError {
-              positives: vec![Rule::data_type],
-              negatives: vec![],
-            },
-            variable.name_span.unwrap(),
-          ));
-        }
-        let mut function = pair.into_inner();
-        let _sep = function.next().unwrap();
-        let function_name = function.next().unwrap();
-        variable
-          .functions
-          .push((function_name.as_str(), function_name.as_span()));
-      },
-      Rule::pair_close => {
-        variable.location =
-          (open_pairs.as_span().start(), pair.as_span().end());
-        break;
-      },
-      _ => {},
-    };
-  }
-  // data type check
-  if variable.typed && variable.data_type.is_none() {
-    return Err(PestError::new_from_span(
-      ErrorVariant::ParsingError {
-        positives: vec![Rule::data_type],
-        negatives: vec![],
-      },
-      variable.name_span.clone().unwrap(),
-    ));
-  }
-  Ok(variable)
-}
-
-fn parse_data_type<'b>(pair: &Pair<Rule>) -> SigmaResult<'b, DataType> {
-  use self::DataType::*;
-  let val = pair.as_str();
-  let result = match val {
-    "u8" => U8,
-    "i8" => I8,
-    "u16" => U16,
-    "i16" => I16,
-    "u32" => U32,
-    "i32" => I32,
-    "u64" => U64,
-    "i64" => I64,
-    "f32" => F32,
-    "f64" => F64,
-    "bool" => Bool,
-    "str" => Str,
-    _ => {
-      return Err(PestError::new_from_span(
-        ErrorVariant::CustomError {
-          message: format!("Unknown Data type: {}", val),
+  // TODO: Refactor this function
+  // TODO: Validate Datatypes
+  // TODO: Parse functions in seprate function
+  fn parse_var_pair(&mut self, pair: Pair<'s, Rule>) -> SigmaResult<()> {
+    let mut variable = Variable::default();
+    let mut inner_rules = pair.into_inner();
+    let open_pairs = inner_rules.next().unwrap();
+    let var = inner_rules.next().unwrap();
+    let var_inner = var.into_inner();
+    for var_rules in var_inner {
+      match var_rules.as_rule() {
+        Rule::nullable => {
+          variable.nullable = true;
         },
-        pair.as_span(),
+        Rule::var_name => {
+          variable.name = var_rules.as_str();
+          variable.name_span = Some(var_rules.as_span());
+        },
+        Rule::data_type_sep => {
+          // it must has data type then
+          variable.typed = true;
+        },
+        Rule::data_type => {
+          let data_type = var_rules;
+          variable.data_type =
+            Some((self.parse_data_type(&data_type)?, data_type.as_span()));
+        },
+        _ => {},
+      };
+    }
+    // data type check
+    if variable.typed && variable.data_type.is_none() {
+      return Err(PestError::new_from_span(
+        ErrorVariant::ParsingError {
+          positives: vec![Rule::data_type],
+          negatives: vec![],
+        },
+        variable.name_span.clone().unwrap(),
       ));
-    },
-  };
-  Ok(result)
+    }
+    let mut variable = self.parse_function(inner_rules, variable)?;
+    variable.location = (open_pairs.as_span().start(), variable.location.1);
+    self.vars.insert(variable.name, variable);
+    Ok(())
+  }
+
+  fn parse_data_type<'b>(
+    &self,
+    pair: &Pair<Rule>,
+  ) -> SigmaResult<'b, DataType> {
+    use self::DataType::*;
+    let val = pair.as_str();
+    let result = match val {
+      "u8" => U8,
+      "i8" => I8,
+      "u16" => U16,
+      "i16" => I16,
+      "u32" => U32,
+      "i32" => I32,
+      "u64" => U64,
+      "i64" => I64,
+      "f32" => F32,
+      "f64" => F64,
+      "bool" => Bool,
+      "str" => Str,
+      _ => {
+        let p_vals = [
+          "u8", "u16", "u32", "u64", "i8", "i16", "i32", "i64", "f32", "f64",
+          "str", "bool",
+        ];
+        let mut extra_help = String::new();
+        if let Some(matches) = parser::did_you_mean(val, p_vals.iter()) {
+          extra_help = format!("Did you mean: `{}` ?", matches);
+        }
+        return Err(PestError::new_from_span(
+          ErrorVariant::CustomError {
+            message: format!("unknown Data type: `{}` {}", val, extra_help),
+          },
+          pair.as_span(),
+        ));
+      },
+    };
+    Ok(result)
+  }
+
+  fn parse_function<'f>(
+    &self,
+    pairs: Pairs<'f, Rule>,
+    mut var: Variable<'f>,
+  ) -> SigmaResult<'f, Variable<'f>> {
+    for pair in pairs {
+      let rule = pair.as_rule();
+      match rule {
+        Rule::function => {
+          if var.data_type.is_none() || !var.typed {
+            return Err(PestError::new_from_span(
+              ErrorVariant::ParsingError {
+                positives: vec![Rule::data_type],
+                negatives: vec![],
+              },
+              var.name_span.unwrap(),
+            ));
+          }
+          let mut function = pair.into_inner();
+          let _sep = function.next().unwrap();
+          let function_name = function.next().unwrap();
+          if !self.functions.contains_key(function_name.as_str()) {
+            let mut extra_help = String::new();
+            if let Some(matches) =
+              parser::did_you_mean(function_name.as_str(), self.functions.keys())
+            {
+              extra_help = format!("Did you mean: `{}` ?", matches);
+            }
+            return Err(PestError::new_from_span(
+              ErrorVariant::CustomError {
+                message: format!(
+                  "undefined function: {} {}",
+                  function_name.as_str(),
+                  extra_help
+                ),
+              },
+              function_name.as_span(),
+            ));
+          }
+          var
+            .functions
+            .push((function_name.as_str(), function_name.as_span()));
+        },
+        Rule::pair_close => {
+          var.location = (0, pair.as_span().end());
+          break;
+        },
+        _ => {},
+      };
+    }
+    Ok(var)
+  }
 }
 
 #[cfg(test)]
--- src/parser.rs
+++ src/parser.rs
@@ -6,3 +6,42 @@ use pest_derive::Parser;
 #[derive(Parser)]
 #[grammar = "sigma.pest"]
 pub(crate) struct SigmaParser;
+
+/// Produces a string from a given list of possible values which is similar to
+/// the passed in value `v` with a certain confidence.
+/// Thus in a list of possible values like ["foo", "bar"], the value "fop" will
+/// yield `Some("foo")`, whereas "blark" would yield `None`.
+/// see: https://github.com/clap-rs/clap/blob/master/src/suggestions.rs
+pub(crate) fn did_you_mean<'a, T: ?Sized, I>(
+  v: &str,
+  possible_values: I,
+) -> Option<&'a str>
+where
+  T: AsRef<str> + 'a,
+  I: IntoIterator<Item = &'a T>,
+{
+  let mut candidate: Option<(f64, &str)> = None;
+  for pv in possible_values {
+    let confidence = strsim::jaro_winkler(v, pv.as_ref());
+    if confidence > 0.8
+      && (candidate.is_none() || (candidate.as_ref().unwrap().0 < confidence))
+    {
+      candidate = Some((confidence, pv.as_ref()));
+    }
+  }
+  match candidate {
+    None => None,
+    Some((_, candidate)) => Some(candidate),
+  }
+}
+
+#[cfg(test)]
+mod test_did_you_mean {
+  use super::*;
+
+  #[test]
+  fn possible_values_match() {
+    let p_vals = ["test", "possible", "values"];
+    assert_eq!(did_you_mean("tst", p_vals.iter()), Some("test"));
+  }
+}
